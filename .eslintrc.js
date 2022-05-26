3eef0024728ea65fb4d89c5992433400fed0001f
Sebastian Johansson <sebastian.johansson@revolutionrace.se>
2022-05-26 14:28:07 +0200

    Adds better eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,33 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    browser: true
+    es6: true,
+    browser: true,
   },
   extends: [
-    'plugin:vue-scoped-css/vue3-recommended',
+    "@nuxtjs/eslint-config-typescript",
+    /* 'plugin:vue-scoped-css/vue3-recommended',
     'plugin:vue/essential',
-    'plugin:vue/vue3-recommended',
-    '@vue/typescript/recommended',
+    'plugin:vue/vue3-recommended', */
+    "@vue/typescript/recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
   ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
   rules: {
-    'max-len': ["error", { "code": 240 }]                                                                                                                                
+    "max-len": ["error", { code: 240 }],
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.test.ts", "**/*.test.tsx"] },
+    ],
   },
   globals: {
-    $nuxt: true
+    $nuxt: true,
+  },
+  settings: {
+    "import/core-modules": ["vue", "vuex", "vue-meta", "axios"], // these modules are included in nuxt.js
   },
 };
