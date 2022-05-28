49830ec4ab8fe3e2e0d61d0724f7a4631d843108
Sebastian Johansson <sebbe.johansson1995@gmail.com>
2022-05-28 23:04:07 +0200

    Migrates plugin media-handler to nuxt3 structure.

--- plugins/media-handler.ts
+++ plugins/media-handler.ts
@@ -1,4 +1,30 @@
-import { Plugin } from '@nuxt/types';
+export default defineNuxtPlugin(useNuxtApp => {
+  interface MediaArguments {
+    maxHeight: number;
+    maxWidth: number;
+    skipAutoFormat?: boolean | undefined;
+  }
+
+  function toMediaUrl(slug: string, { maxHeight, maxWidth, skipAutoFormat = false }: MediaArguments) {
+    const qs = [];
+
+    if (!skipAutoFormat) {
+      qs.push('auto=format');
+    }
+
+    let imageUrl = `https://sebbejohansson.imgix.net/${slug}`;
+
+    if (qs.length) {
+      imageUrl += `?${qs.join('&')}`;
+    }
+
+    return imageUrl;
+  }
+  // accessible directly with useNuxtApp()
+  useNuxtApp.provide('toMediaUrl', toMediaUrl);
+})
+
+/* import { Plugin } from '@nuxt/types';
 
 declare module '@nuxt/types' {
   interface NuxtAppOptions {
@@ -9,39 +35,11 @@ declare module '@nuxt/types' {
   }
 }
 
-interface MediaArguments {
-  maxHeight: number;
-  maxWidth: number;
-  skipAutoFormat?: boolean | undefined;
-}
-
-function toMediaUrl(slug: string, { maxHeight, maxWidth, skipAutoFormat = false }: MediaArguments) {
-  const qs = [];
-
-  /* if (maxWidth) {
-    qs.push(`maxWidth=${Math.round(maxWidth)}`);
-  }
-
-  if (maxHeight) {
-    qs.push(`maxHeight=${Math.round(maxHeight)}`);
-  } */
-
-  if (!skipAutoFormat) {
-    qs.push('auto=format');
-  }
-
-  let imageUrl = `https://sebbejohansson.imgix.net/${slug}`;
-
-  if (qs.length) {
-    imageUrl += `?${qs.join('&')}`;
-  }
-
-  return imageUrl;
-}
-
 const mediaHandler: Plugin = (context, inject) => {
   context.$toMediaUrl = toMediaUrl;
   inject('toMediaUrl', toMediaUrl);
 };
 
 export default mediaHandler;
+
+*/
