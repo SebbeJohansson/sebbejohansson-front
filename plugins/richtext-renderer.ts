ebf9298a8fff2f43da2face856136a4a3f1a12b2
Sebastian Johansson <sebbe.johansson1995@gmail.com>
2022-06-21 23:56:12 +0200

    Removes unused plugin for richtext rendering.

--- plugins/richtext-renderer.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import { plugin } from '@marvr/storyblok-rich-text-vue-renderer'
-
-export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(plugin(/* options */))
-})
