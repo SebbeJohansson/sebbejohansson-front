6ea60c66f4ed9d5b11028edb09c3acbb21e49583
Sebastian Johansson <sebbe.johansson1995@gmail.com>
2022-08-21 17:39:40 +0200

    Adds test for GithubGist.

---
+++ plugins/vue-github-gist-renderer.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import VueEmbedGist from 'vue-embed-gist';
+
+Vue.component('VueEmbedGist', VueEmbedGist);
---
+++ storyblok/GithubGist.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import VueEmbedGist from "vue-embed-gist";
+
+
+const props = defineProps({ blok: Object });
+</script>
+
+<template>
+  <div v-editable="blok" class="github-gist">
+    {{ blok }}
+    <vue-embed-gist gist-id="9a6b863e3a3cfc0a79df457b0392b5d5" file="StoryblokDateTimeField.js" />
+  </div>
+</template>
+
+<style>
+.text img {
+  max-width: 100%;
+}
+</style>
